886576e3229464e1fd02ad496c4d1e80a8ed9385
wangpan <292519413@qq.com>
2021-06-12 14:19:55 +0800

    优化米家去广告模块

--- app/src/main/java/com/beforecar/ad/policy/MiJiaHookPolicy.kt
+++ app/src/main/java/com/beforecar/ad/policy/MiJiaHookPolicy.kt
@@ -1,66 +1,77 @@
 package com.beforecar.ad.policy
 
 import android.app.Application
+import com.beforecar.ad.okhttp.OkHttpHelper
 import com.beforecar.ad.policy.base.AbsHookPolicy
 import com.beforecar.ad.policy.base.getStackInfo
 import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XposedBridge
+import java.io.IOException
 
 /**
  * @author: wangpan
  * @email: p.wang@aftership.com
  * @date: 2021/5/6
+ *
  * 米家
  */
 class MiJiaHookPolicy : AbsHookPolicy() {
 
     override val tag: String = "tag_mijia"
 
+    private var okHttpHelper: OkHttpHelper? = null
+
     override fun getPackageName(): String {
         return "com.xiaomi.smarthome"
     }
 
-    override fun onMainApplicationAfterCreate(application: Application, classLoader: ClassLoader) {
-        removeHomePageAd(classLoader)
-        removeMinePageAd(classLoader)
+    override fun onMainApplicationBeforeCreate(application: Application, classLoader: ClassLoader) {
+        //每次启动重置
+        okHttpHelper = null
+        //hook RealCall
+        hookRealCall(classLoader)
     }
 
-    /**
-     * 移除米家首页 Banner 广告
-     */
-    private fun removeHomePageAd(classLoader: ClassLoader) {
-        try {
-            log("removeHomePageAd start")
-            XposedHelpers.findAndHookMethod(
-                "_m_j.hdy", classLoader, "O000000o", String::class.java, object : XC_MethodHook() {
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        param.result = emptyList<Any>()
-                        log("removeHomePageAd success")
-                    }
-                }
-            )
-        } catch (t: Throwable) {
-            log("removeHomePageAd fail: ${t.getStackInfo()}")
+    private fun getOkHttpHelper(): OkHttpHelper {
+        return okHttpHelper ?: kotlin.run {
+            OkHttpHelper.create(
+                realCall = "okhttp3.RealCall",
+                getResponseWithInterceptorChain = "getResponseWithInterceptorChain",
+                getRequest = "request"
+            ).also {
+                okHttpHelper = it
+            }
         }
     }
 
     /**
-     * 移除我的页面广告
+     * hook RealCall
      */
-    private fun removeMinePageAd(classLoader: ClassLoader) {
+    private fun hookRealCall(classLoader: ClassLoader) {
         try {
-            log("removeMinePageAd start")
-            XposedHelpers.findAndHookMethod(
-                "_m_j.hea", classLoader, "O000000o", String::class.java,
+            XposedBridge.hookMethod(
+                getOkHttpHelper().getResponseWithInterceptorChainMethod(classLoader),
                 object : XC_MethodHook() {
                     override fun beforeHookedMethod(param: MethodHookParam) {
-                        param.result = emptyList<Any>()
-                        log("removeMinePageAd success")
+                        val realCall = param.thisObject as Any
+                        val url = getOkHttpHelper().getUrl(realCall)
+                        when {
+                            //首页 banner 广告
+                            url.contains("/recommendation/banner") -> {
+                                param.throwable = IOException("disable banner ad")
+                                log("removeBannerAd success")
+                            }
+                            //我的页面推荐广告
+                            url.contains("/recommendation/myTab") -> {
+                                param.throwable = IOException("disable myTab ad")
+                                log("removeMyTabAd success")
+                            }
+                        }
                     }
                 }
             )
         } catch (t: Throwable) {
-            log("removeMinePageAd fail: ${t.getStackInfo()}")
+            log("hookRealCall fail: ${t.getStackInfo()}")
         }
     }
 
