441a22acab8537527cd53822ce7a4896ca75c227
wangpan <292519413@qq.com>
2021-06-20 21:40:58 +0800

    新增酷安去广告模块

--- app/src/main/java/com/beforecar/ad/policy/CoolApkHookPolicy.kt
+++ app/src/main/java/com/beforecar/ad/policy/CoolApkHookPolicy.kt
@@ -3,6 +3,7 @@ package com.beforecar.ad.policy
 import android.app.Application
 import android.content.Context
 import com.beforecar.ad.policy.base.AbsHookPolicy
+import com.beforecar.ad.policy.base.getProcessName
 import com.beforecar.ad.policy.base.getStackInfo
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
@@ -30,28 +31,12 @@ class CoolApkHookPolicy : AbsHookPolicy() {
 
     private fun hookMyWrapperProxyApplication(classLoader: ClassLoader) {
         try {
+            val appCls = XposedHelpers.findClass(MyWrapperProxyApplication, classLoader)
             XposedHelpers.findAndHookMethod(
-                MyWrapperProxyApplication, classLoader,
-                "initProxyApplication", Context::class.java,
-                object : XC_MethodHook() {
+                appCls, "initProxyApplication", Context::class.java, object : XC_MethodHook() {
                     override fun afterHookedMethod(param: MethodHookParam) {
                         val application = param.thisObject as Application
-                        //hook 今日头条穿山甲 SDK
-                        //hookHttpStack(application.classLoader!!)
                         hookCoolMarketApplication(application.classLoader!!)
-                    }
-
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        val application = param.thisObject as Application
-                        try {
-                            val mainActivity = XposedHelpers.findClass(
-                                "com.coolapk.market.view.main.MainActivity",
-                                application.classLoader
-                            )
-                            log("mainActivity success")
-                        } catch (t: Throwable) {
-                            log("mainActivity fail")
-                        }
                     }
                 }
             )
@@ -62,62 +47,76 @@ class CoolApkHookPolicy : AbsHookPolicy() {
 
     private fun hookCoolMarketApplication(classLoader: ClassLoader) {
         try {
-            XposedHelpers.findAndHookMethod(
-                CoolMarketApplication, classLoader, "onCreate", object : XC_MethodHook() {
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        val application = param.thisObject as Application
-                        //hook 今日头条穿山甲 SDK
-                        hookHttpStack(application.classLoader!!)
-                    }
-
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-
-                    }
-
+            val appCls = XposedHelpers.findClass(CoolMarketApplication, classLoader)
+            XposedHelpers.findAndHookMethod(appCls, "onCreate", object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val application = param.thisObject as Application
+                    onCoolMarketApplicationCreate(application, application.classLoader!!)
                 }
-            )
+            })
         } catch (t: Throwable) {
             log("hookCoolMarketApplication fail: ${t.getStackInfo()}")
         }
     }
 
+    private fun onCoolMarketApplicationCreate(application: Application, classLoader: ClassLoader) {
+        if (application.getProcessName() != getPackageName()) return
+        //hook 穿山甲 SDK
+        hookTTAdSdk(classLoader)
+        //hook 广点通 SDK
+        hookGDTAdSDK(classLoader)
+    }
+
     /**
      * hook 今日头条穿山甲 SDK
      */
-    private fun hookHttpStack(classLoader: ClassLoader) {
+    private fun hookTTAdSdk(classLoader: ClassLoader) {
         try {
-            val httpStackCls = XposedHelpers.findClass(HttpStackImpl, classLoader)
-            val requestCls = XposedHelpers.findClass(Request, classLoader)
+            val ttAdConfigCls = XposedHelpers.findClass(TTAdConfig, classLoader)
             XposedHelpers.findAndHookMethod(
-                httpStackCls, "performRequest",
-                requestCls, Map::class.java,
+                ttAdConfigCls, "setAppId",
+                String::class.java,
                 object : XC_MethodHook() {
                     override fun beforeHookedMethod(param: MethodHookParam) {
-                        val request = param.args[0] ?: return
-                        val url = getUrlFromRequest(request)
-                        log("url: $url")
+                        val appId = param.args[0] as String
+                        param.args[0] = System.currentTimeMillis().toString()
+                        log("hookTTAdSdk success: $appId")
                     }
                 }
             )
         } catch (t: Throwable) {
-            log("hookHttpStack fail: ${t.getStackInfo()}")
+            log("hookTTAdSdk fail: ${t.getStackInfo()}")
         }
     }
 
-    private fun getUrlFromRequest(request: Any): String {
+    private fun hookGDTAdSDK(classLoader: ClassLoader) {
         try {
-            return XposedHelpers.callMethod(request, "getUrl") as? String ?: ""
+            val gdtADManagerCls = XposedHelpers.findClass(GDTADManager, classLoader)
+            XposedHelpers.findAndHookMethod(
+                gdtADManagerCls, "initWith",
+                Context::class.java, String::class.java,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        val appId = param.args[1] as String
+                        param.args[1] = System.currentTimeMillis().toString()
+                        log("hookGDTAdSDK success: $appId")
+                    }
+                }
+            )
         } catch (t: Throwable) {
-            log("getUrlFromRequest fail: ${t.getStackInfo()}")
+            log("hookGDTAdSDK fail: ${t.getStackInfo()}")
         }
-        return ""
     }
 
     companion object {
         const val MyWrapperProxyApplication = "com.coolapk.market.MyWrapperProxyApplication"
         const val CoolMarketApplication = "com.coolapk.market.CoolMarketApplication"
-        const val HttpStackImpl = "com.bytedance.sdk.adnet.core.j"
-        const val Request = "com.bytedance.sdk.adnet.core.Request"
+
+        //穿山甲SDK
+        const val TTAdConfig = "com.bytedance.sdk.openadsdk.TTAdConfig"
+
+        //广点通SDK
+        const val GDTADManager = "com.qq.e.comm.managers.GDTADManager"
     }
 
 }
